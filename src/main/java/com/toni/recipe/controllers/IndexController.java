a4741c4c32108324ad1edb6dbd12650247898ab3
toni <ferreirocouto@gmail.com>
2020-01-16 18:11:54 +0100

    edit repositories

--- src/main/java/com/toni/recipe/controllers/IndexController.java
+++ src/main/java/com/toni/recipe/controllers/IndexController.java
@@ -1,13 +1,34 @@
 package com.toni.recipe.controllers;
 
+import com.toni.recipe.domain.Category;
+import com.toni.recipe.domain.UnitOfMeasure;
+import com.toni.recipe.repositories.CategoryRepository;
+import com.toni.recipe.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"","/","/index"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("America");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("teaspoon");
+
+        System.out.println("category id is: "+categoryOptional.get().getId());
+        System.out.println("uom id is: "+unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
--- src/main/java/com/toni/recipe/repositories/CategoryRepository.java
+++ src/main/java/com/toni/recipe/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.toni.recipe.repositories;
 import com.toni.recipe.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
