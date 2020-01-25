14315634a4b708f8662e48066d429cd210b0c4b1
toni <ferreirocouto@gmail.com>
2020-01-25 20:18:01 +0100

    add validation

--- src/test/java/com/toni/recipe/controllers/RecipeControllerTest.java
+++ src/test/java/com/toni/recipe/controllers/RecipeControllerTest.java
@@ -73,6 +73,7 @@ public class RecipeControllerTest {
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .param("id", "")
                 .param("description", "some string")
+                .param("directions", "some directions")
         )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/show/2"));
@@ -117,5 +118,22 @@ public class RecipeControllerTest {
         mockMvc.perform(get("/recipe/show/asdf"))
                 .andExpect(status().isBadRequest())
                 .andExpect(view().name("400error"));
+    }
+
+    @Test
+    public void testPostNewRecipeFormValidationFail() throws Exception {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(2L);
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+        mockMvc.perform(post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+
+        )
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("recipe"))
+                .andExpect(view().name("recipe/recipeForm"));
     }
 }
